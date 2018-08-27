970b8e6086ccd3c6fcf1dde29ca0531f3d14ef10
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-27 15:24:46 -0400

    Fixed!
    need to remember to actually create the list

--- TAGenericsDrill/Program.cs
+++ TAGenericsDrill/Program.cs
@@ -12,25 +12,27 @@ namespace TAGenericsDrill
         static void Main(string[] args)
         {           
             Employee<string> emplString = new Employee<string>();
+            emplString.things = new List<string>();
             emplString.things.Add("screws");
             emplString.things.Add("bolts");
             emplString.things.Add("things");
             emplString.things.Add("stuff");
 
-            foreach (string emplStrings in emplString)
+            foreach (string emplStrings in emplString.things)
             {
-                Console.WriteLine(emplString.things);
+                Console.WriteLine(emplString);
             }
 
             Employee<int> emplInt = new Employee<int>();
+            emplInt.things = new List<int>();
             emplInt.things.Add(12);
             emplInt.things.Add(13);
             emplInt.things.Add(14);
             emplInt.things.Add(15);
            
-            foreach (int emplInts in emplInt)
+            foreach (int emplInts in emplInt.things)
             {
-                Console.WriteLine(emplInt.things);
+                Console.WriteLine(emplInt);
             }
             Console.ReadLine();
         }

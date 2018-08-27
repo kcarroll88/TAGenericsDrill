7b6b3ec7085c4b0574e36d65faa8b0c2e3837594
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-27 14:48:41 -0400

    Generics test

--- TAGenericsDrill/Program.cs
+++ TAGenericsDrill/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TAGenericsDrill
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {           
+            Employee<string> emplString = new Employee<string>();
+            emplString.things.Add("screws");
+            emplString.things.Add("bolts");
+            emplString.things.Add("things");
+            emplString.things.Add("stuff");
+
+            foreach (string emplStrings in emplString)
+            {
+                Console.WriteLine(emplString.things);
+            }
+
+            Employee<int> emplInt = new Employee<int>();
+            emplInt.things.Add(12);
+            emplInt.things.Add(13);
+            emplInt.things.Add(14);
+            emplInt.things.Add(15);
+           
+            foreach (int emplInts in emplInt)
+            {
+                Console.WriteLine(emplInt.things);
+            }
+            Console.ReadLine();
         }
     }
 }
